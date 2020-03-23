32e95c79389f811ec39bb0e0ecbf63ab5c0cbd51
linenqi <397952780@qq.com>
2020-03-23 15:40:06 +0800

    ReuseLayoutGroupItem范例修改

--- Assets/ReuseLayoutGroupItems/Samples/FriendList.cs
+++ Assets/ReuseLayoutGroupItems/Samples/FriendList.cs
@@ -19,6 +19,25 @@ namespace Lin {
         // Start is called before the first frame update
         void Start()
         {
+            //m_ReuseScrollRect.InitItems(m_ReuseItem, 100, 90, 0, (index, scrollRectItem) =>
+            //{
+            //    scrollRectItem.GetComponent<FriendItem>().text.text = "Friend " + index;
+            //});
+
+
+            List<float> itemLengths = new List<float>() {
+                50,  150, 300,  100, 110,
+                70,  120, 70,  290,  130,
+                100, 50,  260, 90,  120,
+                270,  60,  90,  350,  190,
+
+            };
+
+            //m_ReuseScrollRect.InitItems(m_ReuseItem, itemLengths, 0, (index, scrollRectItem) =>
+            //{
+            //    scrollRectItem.GetComponent<FriendItem>().text.text = "Friend " + index;
+            //});
+
             m_ReuseScrollRect.InitItems(m_ReuseItem, 100, 90, 0, (index, scrollRectItem) =>
             {
                 scrollRectItem.GetComponent<FriendItem>().text.text = "Friend " + index;
@@ -34,6 +53,7 @@ namespace Lin {
         public void AddItem()
         {
             m_ReuseScrollRect.AddItem();
+            m_ReuseScrollRect.SetContentPosToEnd();
         }
 
         public void RemoveItem()
@@ -46,13 +66,55 @@ namespace Lin {
             m_ReuseScrollRect.RemoveAllItems();
         }
 
-        public void RefreshItem() {
+        public void RefreshItem()
+        {
 
-            int itemCount = int.Parse(m_InputField.text);
+            //int itemCount = int.Parse(m_InputField.text);
 
-            m_ReuseScrollRect.RefreshItems(itemCount);
+            m_ReuseScrollRect.RefreshItems(Random.Range(1, 30));
 
             m_ReuseScrollRect.ResetContentPos();
+
+        }
+
+        //public void AddItem()
+        //{
+        //    m_ReuseScrollRect.AddItem(Random.Range(50, 200));
+        //    m_ReuseScrollRect.SetContentPosToEnd();
+        //}
+
+        //public void RemoveItem()
+        //{
+        //    m_ReuseScrollRect.RemoveItem();
+        //}
+
+        //public void RemoveAllItems()
+        //{
+        //    m_ReuseScrollRect.RemoveAllItems();
+        //}
+
+        //public void RefreshItem()
+        //{
+
+        //    m_ReuseScrollRect.RefreshItems(GetRandomList());
+        //    m_ReuseScrollRect.SetContentPosToEnd();
+        //}
+
+        public static List<int> countList = new List<int>{ 10, 1, 20, 0, 4, 1, 10 };
+        public static int listIndex = 0;
+        public List<float> GetRandomList()
+        {
+            int count = countList[++listIndex >= countList.Count ? 0 : listIndex];// Random.Range(0, 15);
+
+            List<float> itemLengths = new List<float>();
+
+            for (int i = 0; i < count; i++) {
+
+                itemLengths.Add(Random.Range(50, 200));
+            }
+
+            return itemLengths;
+
         }
 
     }
