a9f9c75bb774ca9d3bd7aed9c39acb083854ee0f
linenqi <397952780@qq.com>
2020-03-24 14:23:11 +0800

    生成模板脚本，生成模板代码块范例

---
+++ Assets/ScriptGenerator/Editor/TemplateScriptGenerator/Samples/CodeBlockGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace DevelopTools
+{
+    /// <summary>
+    /// 根据代码块模板创建脚本
+    /// </summary>
+    public static class CodeBlockGenerator
+    {
+        //Hotfix MonoBehaviour
+        [MenuItem(Defitions.ScriptGenerator.MenuDiretoryPrefix + "Template/CodeBlockTemplate", priority = 0)]
+        public static void CreateCodeBlockTemplateScript()
+        {
+            CreateScript(Defitions.ScriptGenerator.SampleFolder + "/CodeBlockTemplate.txt", "NewCodeBlockTemplate.cs");
+        }
+
+        private static void CreateScript(string templateFile, string outputFile)
+        {
+            UnityEngine.Object activeObject = Selection.activeObject;
+            string directory = AssetDatabase.GetAssetPath(activeObject);
+
+            Dictionary<string, string> codeReplaceDict = new Dictionary<string, string>()
+            {
+                ["__Name__"] = "NewCodeBlockTemplate",
+                ["__Test_Code_Block__"] = GetCodeBlockTest(10)
+            };
+
+            TemplateScriptGenerator.GenerateScriptFile(templateFile, directory + "/" + outputFile, codeReplaceDict, Encoding.UTF8);
+
+        }
+
+        private static string GetCodeBlockTest(int count)
+        {
+
+            List<CodeBlock> codeBlocks = new List<CodeBlock>() {
+                    new CodeBlock(){
+                        BlockName = "CodeBlockTest",
+                        CodeReplacementDict = new Dictionary<string, string>(){
+                            ["__Count__"] = "" + count,
+
+                        }
+                    }
+                };
+
+            return GetVariableCode(codeBlocks);
+
+        }
+
+        private static string GetVariableCode(List<CodeBlock> codeBlocks)
+        {
+            return TemplateScriptGenerator.GenerateCodeBlock(Defitions.ScriptGenerator.SampleFolder + "/CodeBlock.txt", codeBlocks);
+
+        }
+    }
+}
---
+++ Assets/ScriptGenerator/Editor/TemplateScriptGenerator/Samples/ScriptGenerator.cs
@@ -0,0 +1,35 @@
+﻿using DevelopTools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace _98duP14
+{
+    /// <summary>
+    /// 根据脚本模板创建脚本
+    /// </summary>
+    public static class ScriptGenerator
+    {
+
+        //MonoBehaviour
+        [MenuItem(Defitions.ScriptGenerator.MenuDiretoryPrefix + "Template/MonoBehaviour", priority = 0)]
+        public static void CreateHotfixMonoBehaviourScript()
+        {
+            CreateScript("NewMonoBehaviour.cs", "MonoBehaviourTemplate.txt");
+        }
+
+        private static void CreateScript(string generateFile, string templateFile) {
+
+            UnityEngine.Object activeObject = Selection.activeObject;
+            string generatedPath = AssetDatabase.GetAssetPath(activeObject);
+
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
+                ScriptableObject.CreateInstance<GenerateScriptsAction>(),
+                generatedPath + "/" + generateFile, EditorGUIUtility.FindTexture("cs Script Icon"),
+                Defitions.ScriptGenerator.SampleFolder + "/" + templateFile);
+        }
+
+    }
+}
