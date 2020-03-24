e4a03e4859a2ba78d84b60a2adb3a0be7b553ddd
linenqi <397952780@qq.com>
2020-03-24 14:28:35 +0800

    常量等定义

---
+++ Assets/ScriptGenerator/Editor/Definitions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DevelopTools {
+    public static partial class Defitions
+    {
+        public static class ScriptGenerator
+        {
+            public static readonly string DirectoryPrefix = "Assets/ScriptGenerator/Editor/";
+            public const string MenuDiretoryPrefix = "Assets/Create/Custom Generate/";
+            public static readonly string ScriptTemplateFolder = DirectoryPrefix + "TemplateScriptGenerator/ScriptTemplates";
+
+            public static readonly string SampleFolder = DirectoryPrefix + "TemplateScriptGenerator/Samples";
+        }
+    }
+
+}
+
